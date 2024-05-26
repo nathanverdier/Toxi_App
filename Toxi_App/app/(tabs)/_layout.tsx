283a896628a46e012c59e06730dbfbdd26e54f17
Nathan <nathanverdier34@gmail.com>
2024-05-26 16:06:20 +0200

    🚧 Update : name and icon

--- Toxi_App/app/(tabs)/_layout.tsx
+++ Toxi_App/app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="addsomeone"
         options={{
-          title: 'Explore',
+          title: 'Addsomeone.tsx',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
